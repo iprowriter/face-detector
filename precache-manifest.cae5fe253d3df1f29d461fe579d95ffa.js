67d54cb4ac240154821b95109ebc65d8cf81672e
iprowriter <46689083+iprowriter@users.noreply.github.com>
2020-05-13 12:44:36 +0200

    Updates

---
+++ precache-manifest.cae5fe253d3df1f29d461fe579d95ffa.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4e23e8ec60f95660de2f7bc20aebfd31",
+    "url": "/face-detector/index.html"
+  },
+  {
+    "revision": "62c40eac4fceb721d051",
+    "url": "/face-detector/static/css/2.036b41f9.chunk.css"
+  },
+  {
+    "revision": "f347dd166e58bafa0706",
+    "url": "/face-detector/static/css/main.52fe3329.chunk.css"
+  },
+  {
+    "revision": "62c40eac4fceb721d051",
+    "url": "/face-detector/static/js/2.50b4c531.chunk.js"
+  },
+  {
+    "revision": "501f379103cbfcb5ff11211dd39a28a2",
+    "url": "/face-detector/static/js/2.50b4c531.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f347dd166e58bafa0706",
+    "url": "/face-detector/static/js/main.b2f002b1.chunk.js"
+  },
+  {
+    "revision": "0cec8e2c8322ef68c027",
+    "url": "/face-detector/static/js/runtime-main.091881bc.js"
+  }
+]);
